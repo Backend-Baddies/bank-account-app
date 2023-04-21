bc30bc2f4867e8112ee7965a8cccd138cfd585ad
ifisher <ifisher@cisco.com>
2023-04-21 15:29:30 -0500

    Requested changes made

--- db/models/index.js
+++ db/models/index.js
@@ -1,21 +1,17 @@
-const { User } = require('./User');
-const { Checking } = require('./Checking');
-const { Saving } = require('./Saving');
+/** @format */
+
+const { User } = require("./User");
+const { Checking } = require("./Checking");
+const { Saving } = require("./Saving");
 
 Checking.belongsTo(User);
 User.hasMany(Checking);
-
-module.exports = {
-    User,
-    Checking
-}
 
 Saving.belongsTo(User);
 User.hasMany(Saving);
 
 module.exports = {
-    User,
-    Saving,
-    Checking
-}
-
+  User,
+  Saving,
+  Checking,
+};

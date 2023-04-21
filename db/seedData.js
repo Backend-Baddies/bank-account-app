e38c38418cb97f612a9036a66a8c47bd654c657c
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-04-21 13:48:30 -0700

    forgot a few lines

--- db/seedData.js
+++ db/seedData.js
@@ -28,18 +28,24 @@ const users = [
         phone_number: "212-650-4323",
         email: "john.jackson@email.com",
         address: "1212 N Howe St Crazytown PA 44444",
+        username: "jojacks456",
+        password: "password1",
     },
     {
         name: "Cornelius Waldorf",
         phone_number: "858-555-2565",
         email: "cwaldorf45@email.com",
         address: "555 W Brooklyn St Crazytown PA 44444",
+        username: "cwald56",
+        password: "nunyab1zn@sS",
     },
     {
         name: "Cruella DeVille",
         phone_number: "218-626-5689",
         email: "cruelladv@email.com",
         address: "8913 E Dalmatian Way Crazytown PA 44444",
+        username: "87crueldv",
+        password: "a_passW0rD1",
     }
 ];
 
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res, next) => {
 app.use((error, req, res, next) => {
     console.error('SERVER ERROR: ', error);
     if (res.statusCode < 400) { res.status(500); }
-    res.send({ error: error.message, name: error.nae, message: error.message });
+    res.send({ error: error.message, name: error.name, message: error.message });
 });
 
 module.exports = app;

addc9815c86026c6bf6a9df1cf7eeac17ffd2990
gonzaana35 <107139179+gonzaana35@users.noreply.github.com>
2023-04-21 16:42:29 -0400

    Created User model (#26)

---
+++ db/models/User.js
@@ -0,0 +1,12 @@
+const {sequelize} = require('../db');
+const Sequelize = require('sequelize');
+
+const User = sequelize.define('user', {
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    address: Sequelize.STRING,
+    email: Sequelize.STRING,
+    phone_number: Sequelize.INTEGER
+})
+
+module.exports = {User};
